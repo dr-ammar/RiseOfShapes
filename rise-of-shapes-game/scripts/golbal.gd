0cf8c4e9b61896d34ce7a6334af2e4c1a0e90f24
Ammar Alrashed <ammar.rar2@gmail.com>
2026-04-26 13:52:46 +0300

    my some changes with my macbook

---
+++ rise-of-shapes-game/scripts/golbal.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# GameScheme
+var gamepad_mode = false
+var mobile_mode = false
---
+++ rise-of-shapes-game/scripts/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+	
+
+func _physics_process(delta):
+	get_input()
+	camera_movement()
+	move_and_slide()
+
+
+func get_input():
+	# Keyboard inputs
+	if Global.gamepad_mode == false and Global.mobile_mode == false :
+		var input_dir = Input.get_vector("move_left","move_right","move_up","move_down")
+		velocity = input_dir.normalized() * SPEED
+		
+	if Input.is_action_pressed("move_left"):
+		$characterSprite/Head.flip_h = true
+		$characterSprite/left_foot.flip_h = true
+		$characterSprite/right_foot.flip_h = true
+	elif Input.is_action_pressed("move_right"):
+		$characterSprite/Head.flip_h = false
+		$characterSprite/left_foot.flip_h = false
+		$characterSprite/right_foot.flip_h = false
+	
+	# Mouse and gamepad triggers Clicks
+	#if Input.get_action_strength("shoot") and map.player_deads == false and MobileMod == false:
+	#	get_node("gun_" + str(gun_handled)).shoot.emit()
+
+# Camera smooth movement with mouse
+func camera_movement():
+	if Global.gamepad_mode == false and Global.mobile_mode == false:
+		var mouse_pos = get_global_mouse_position()
+		$Camera2D.offset.x = (mouse_pos.x - global_position.x) / (160.0 / 2.0)
+		$Camera2D.offset.y = (mouse_pos.y - global_position.y) / (90.0 / 2.0)
+	elif Global.gamepad_mode == true and Global.mobile_mode == false:
+		var gamepad_mouse_pos = $gamepad_crosshair.crosshair.global_position
+		$Camera2D.offset.x = (gamepad_mouse_pos.x - global_position.x) / (160.0 / 2.0)
+		$Camera2D.offset.y = (gamepad_mouse_pos.y - global_position.y) / (90.0 / 2.0)
